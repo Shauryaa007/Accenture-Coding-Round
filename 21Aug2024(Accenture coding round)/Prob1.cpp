851a1755a0c9ce550dee3cd25e6a9fedde2082f2
SHAURYA AWASTHI <89693063+Shauryaa007@users.noreply.github.com>
2024-08-23 22:25:17 +0530

    update

--- 21Aug2024(Accenture coding round)/Prob1.cpp
+++ 21Aug2024(Accenture coding round)/Prob1.cpp
@@ -25,29 +25,3 @@ int main()
     
     return 0;
 }
-#include <bits/stdc++.h>
-using namespace std;
-
-int solve(int n)
-{
-    if(n==0||n==1)return n;
-    int pre1=1,pre2=0;
-    
-    for(int i=2;i<=n;i++)
-    {
-        int curr=pre1+pre2;
-        pre2=pre1;
-        pre1=curr;
-    }
-    return pre1;
-}
-
-int main() 
-{
-    int n;
-    cin>>n;
-    
-    cout<<solve(n);
-    
-    return 0;
-}
