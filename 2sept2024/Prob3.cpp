ad09de86af9a445ec1bb2847523e954bf7bcf666
Pranay Mate <113767901+Pranaymate123@users.noreply.github.com>
2024-09-17 15:16:36 +0530

    Update Prob3.cpp
    Hi Shaurya Bhaiyya ,
    I am Pranay and While practising this problem I found a little bug in it. The problem statement clearly says that you can buy at most one box per day. So this code is failing for the test case N=1, D=2, and E=10; So I made the changes accordingly.
    Plz take this pull request cross verify and then  update the code.
    Also in last while calculating the req for days if the left sweets are greater as compared to the req of days then just return ans;
    Ok Thank you

--- 2sept2024/Prob3.cpp
+++ 2sept2024/Prob3.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int solve(int n, int e, int d)
 {
     if(n==0)return -1;
+
+    if(e>n) return -1;// This is because we can Buy the at most one box each day
+    //And If the Need per day is greater than n then simply return -1;
     if(e==0 || d==0)return 0;
     if(d<7) return ((d*e)%n==0)? d*e/n : d*e/n +1;
     if(n*6<e*7)return -1;
@@ -23,10 +26,10 @@ int solve(int n, int e, int d)
     int days= d%7;
     
     int req=(days*e-left);
-    
+    if(req<=0) return ans;   // This is because if left number of sweets greater than requirement so dont need to buy the more boxes of sweets 
     ans+= (req%n == 0 )? (req/n): (req/n)+1;
     return ans;
-    
+    //Thanks
 }
 
 int main() {
