b397f3b160ec2df36ecf7ea5fc89e4b0b8d443e7
SHAURYA AWASTHI <89693063+Shauryaa007@users.noreply.github.com>
2024-09-21 20:28:54 +0530

    19-1

--- 19sept2024/Floyd's.cpp
+++ 19sept2024/Floyd's.cpp
@@ -1,4 +1,9 @@
-int ans = 0;
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int ans = 0;
         for (int i=0; i<N; i++) {
             for (int j=0; j<=i; j++) {
                 ans++;
@@ -6,4 +11,7 @@ int ans = 0;
             }
             cout << "\n";
         }
+
+    return 0;
+}
 
